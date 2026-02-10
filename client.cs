63f6747b2a6f601df15e41133f9007373912188d
blqdema1l <faregreg026@gmail.com>
2026-02-10 18:49:51 +0300

    Update client.cs

--- client.cs
+++ client.cs
@@ -1,15 +1,22 @@
 using System.Net.Sockets;
+using System.Windows.Forms;
 
 namespace Client_winforms
 {
     public partial class Form1 : Form
     {
+
         private TcpClient client;
         private NetworkStream stream;
         private Thread receiveThread;
+
         private bool isConnected = false;
         private string serverIP = "46.226.106.127";
-        private int port = 34987;
+        private int port = 36257;
+        //private string serverIP = "127.0.0.1";
+        //private int port = 27015;
+
+
 
         public Form1()
         {
@@ -44,5 +51,35 @@ namespace Client_winforms
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            if (isConnected && stream != null)
+            {
+                try
+                {
+                    string message = textBox1.Text;
+                    byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+                    label1.Text = "Отправлено: " + message;
+                    stream.Write(data, 0, data.Length);
+                    textBox1.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка отправки: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("...");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
+
 }
