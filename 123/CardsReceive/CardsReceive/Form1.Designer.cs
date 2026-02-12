89327826c04d5ee2cd349b11f1ae7826836517d1
Seliss <130401679+SummerCat52@users.noreply.github.com>
2026-02-12 18:12:12 +0300

    Add files via upload

---
+++ 123/CardsReceive/CardsReceive/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace CardsReceive
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLoadCards = new Button();
+            flowLayoutPanelPlayer = new FlowLayoutPanel();
+            lblRemainingCards = new Label();
+            flowLayoutPanelOpponent = new FlowLayoutPanel();
+            flowLayoutPanelTable = new FlowLayoutPanel();
+            btnPlayCard = new Button();
+            SuspendLayout();
+            // 
+            // btnLoadCards
+            // 
+            btnLoadCards.Location = new Point(3, 12);
+            btnLoadCards.Name = "btnLoadCards";
+            btnLoadCards.Size = new Size(75, 23);
+            btnLoadCards.TabIndex = 0;
+            btnLoadCards.Text = "Card";
+            btnLoadCards.UseVisualStyleBackColor = true;
+            btnLoadCards.Click += btnLoadCards_Click;
+            // 
+            // flowLayoutPanelPlayer
+            // 
+            flowLayoutPanelPlayer.Location = new Point(105, 571);
+            flowLayoutPanelPlayer.Name = "flowLayoutPanelPlayer";
+            flowLayoutPanelPlayer.Size = new Size(645, 202);
+            flowLayoutPanelPlayer.TabIndex = 1;
+            // 
+            // lblRemainingCards
+            // 
+            lblRemainingCards.AutoSize = true;
+            lblRemainingCards.Location = new Point(3, 758);
+            lblRemainingCards.Name = "lblRemainingCards";
+            lblRemainingCards.Size = new Size(38, 15);
+            lblRemainingCards.TabIndex = 2;
+            lblRemainingCards.Text = "label1";
+            // 
+            // flowLayoutPanelOpponent
+            // 
+            flowLayoutPanelOpponent.Location = new Point(105, -3);
+            flowLayoutPanelOpponent.Name = "flowLayoutPanelOpponent";
+            flowLayoutPanelOpponent.Size = new Size(645, 209);
+            flowLayoutPanelOpponent.TabIndex = 3;
+            // 
+            // flowLayoutPanelTable
+            // 
+            flowLayoutPanelTable.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            flowLayoutPanelTable.Location = new Point(149, 274);
+            flowLayoutPanelTable.Name = "flowLayoutPanelTable";
+            flowLayoutPanelTable.Size = new Size(531, 212);
+            flowLayoutPanelTable.TabIndex = 4;
+            // 
+            // btnPlayCard
+            // 
+            btnPlayCard.Location = new Point(474, 513);
+            btnPlayCard.Name = "btnPlayCard";
+            btnPlayCard.Size = new Size(75, 23);
+            btnPlayCard.TabIndex = 5;
+            btnPlayCard.Text = "Атаковать";
+            btnPlayCard.UseVisualStyleBackColor = true;
+            btnPlayCard.Click += btnPlayCard_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 782);
+            Controls.Add(btnPlayCard);
+            Controls.Add(flowLayoutPanelTable);
+            Controls.Add(flowLayoutPanelOpponent);
+            Controls.Add(lblRemainingCards);
+            Controls.Add(flowLayoutPanelPlayer);
+            Controls.Add(btnLoadCards);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnLoadCards;
+        private FlowLayoutPanel flowLayoutPanelPlayer;
+        private Label lblRemainingCards;
+        private FlowLayoutPanel flowLayoutPanelOpponent;
+        private FlowLayoutPanel flowLayoutPanelTable;
+        private Button btnPlayCard;
+    }
+}
---
+++ 123/CardsReceive/CardsReceive/Models.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsReceive
+{
+    public class DeckResponse
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+    }
+
+    public class DrawResponse
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+        public Card[] cards { get; set; }
+    }
+
+    public enum Suit { Clubs, Diamonds, Hearts, Spades }
+
+    public class Card
+    {
+        public string value { get; set; }
+        public string suit { get; set; }
+        public string image { get; set; }
+        public string code { get; set; }
+
+        public Suit SuitEnum
+        {
+            get
+            {
+                return suit switch
+                {
+                    "CLUBS" => Suit.Clubs,
+                    "DIAMONDS" => Suit.Diamonds,
+                    "HEARTS" => Suit.Hearts,
+                    "SPADES" => Suit.Spades,
+                    _ => throw new Exception("Неизвестная масть"),
+                };
+            }
+        }
+    }
+
+    public class Player
+    {
+        public string Id { get; set; }
+        public List<Card> Cards { get; set; } = new List<Card>();
+    }
+    public class GameState
+    {
+        public bool IsPlayerTurn { get; set; }
+        public List<Card> TableCards { get; set; } = new List<Card>();
+        public List<Card> OpponentCards { get; set; } = new List<Card>();
+    }
+}
